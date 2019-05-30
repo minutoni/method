6d4389f637e3d45a9f44aa2104234e51400ce0b1
minutoni <minutoni105@gmail.com>
2019-05-30 11:40:26 +0900

    Initial Commit

---
+++ method/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  method
+//
+//  Created by 所　紀彦 on 2019/05/30.
+//  Copyright © 2019 所　紀彦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
