4911c5c74c7be299887b003e2628b77170ac6465
minutoni <minutoni105@gmail.com>
2019-06-07 10:54:54 +0900

    メソッド

--- method/ViewController.swift
+++ method/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var maeText:UITextField!
+    
+    @IBOutlet var atoText:UITextField!
+    
+    @IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func keisan(){
+        kekka()
+        
+    }
+    
+    func kekka(){
+           var x1 = Int(maeText.text!)!
+        
+        
+        var x2 = Int(atoText.text!)!
+        
+        var number = x1 + x2
+        label.text = String(number)
+    }
 
 }
 
